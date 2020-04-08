eea9efe166c11b268b55cdd5957f78a8696235ef
Dakpaneel <bradleyoosterveen@gmail.com>
2020-04-08 10:25:50 +0200

    Form final file structure

--- index.php
+++ index.php
@@ -1,30 +1,12 @@
 <?php
-    $url = array_slice(explode("/", $_SERVER['REQUEST_URI']), 1);
+    /**
+     * All requests are routed through here.
+     */
 
-    var_dump($url[0]);
-
-    switch($url[0]) {
-
-        case 'api':
-            $next = 'pages/api.php';
-            break;
-        case '':
-            $next = 'pages/docs.php';
-        break;
-
-        default:
-            $next = 'pages/docs.php';
-        break;
-    }
-?>
-
-<?php echo 'index.php'; ?>
-
-<ul>
-    <li><a href="/">Docs</a></li>
-    <li><a href="/api">Api</a></li>
-</ul>
-
-<?php
-    require($next);
+    /**
+     * Bootstrap
+     * 
+     * Loads in all required php functions.
+     */
+    require('assets/bootstrap.php');
 ?>
