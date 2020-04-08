745b0bc1954527e309d841a3f08d734c1af86cb9
Dakpaneel <bradleyoosterveen@gmail.com>
2020-04-08 10:14:55 +0200

    Add barebones routing

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
+<?php
+    $url = array_slice(explode("/", $_SERVER['REQUEST_URI']), 1);
+
+    var_dump($url[0]);
+
+    switch($url[0]) {
+
+        case 'api':
+            $next = 'pages/api.php';
+            break;
+        case '':
+            $next = 'pages/docs.php';
+        break;
+
+        default:
+            $next = 'pages/docs.php';
+        break;
+    }
+?>
+
 <?php echo 'index.php'; ?>
+
+<ul>
+    <li><a href="/">Docs</a></li>
+    <li><a href="/api">Api</a></li>
+</ul>
+
+<?php
+    require($next);
+?>
