1ecb447937bd9177d26098a5ec3c8a23a4b20a57
Dakpaneel <bradleyoosterveen@gmail.com>
2020-04-18 18:49:01 +0200

    Remove comments

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,15 +1,4 @@
 <?php
-//    $url = array_slice(explode("?", $_SERVER['REQUEST_URI']), 0);
-//
-//    switch($url[0]) {
-//        case '':
-//            $response = 'pages/index.php';
-//            break;
-//
-//        default:
-//            $response = 'pages/error/404.php';
-//        break;
-//    }
     class Router
     {
         public $page;
