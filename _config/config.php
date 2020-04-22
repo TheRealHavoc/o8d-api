112d9174a901dda9d6a7cf87ee20a8f49dd2276a
Dakpaneel <bradleyoosterveen@gmail.com>
2020-04-22 10:46:29 +0200

    F*ck it dit is makkelijker

---
+++ _config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('SQL_HOST', '185.227.81.30');
+    define('SQL_DATABASE', 'runecher_ouderavond');
+    define('SQL_USER', 'runecher_ouderavond');
+    define('SQL_PASSWORD', 'Eq1rgNmkJr');
+
+    define('APP_KEY', '');
+?>
