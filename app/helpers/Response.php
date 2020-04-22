0f07856c55aeb810606c554de304e354bc03b575
Dakpaneel <bradleyoosterveen@gmail.com>
2020-04-22 09:41:46 +0200

    Update response helper to allow custom errors

--- app/helpers/Response.php
+++ app/helpers/Response.php
@@ -5,16 +5,34 @@
          * @param $data
          * @param int $http_response_code
          */
-        public static function go($data, $http_response_code = 200)
+        public static function success($data, $http_response_code = 200)
+        {
+            self::headers($http_response_code);
+
+            $data = ["data" => $data];
+
+            echo json_encode($data);
+
+            die;
+        }
+
+        public static function error($error, $http_response_code)
+        {
+            self::headers($http_response_code);
+
+            $error = ["error" => ["code" => $http_response_code, "messages" => $error]];
+
+            echo json_encode($error);
+
+            die;
+        }
+
+        private static function headers($http_response_code)
         {
             header("Access-Control-Allow-Origin: *");
             header("Content-Type: application/json; charset=UTF-8");
 
             http_response_code($http_response_code);
-
-            echo json_encode($data);
-
-            die;
         }
     }
 ?>
