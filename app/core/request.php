4dabe0715521875678f255e24a39a1e780a9fa08
Dakpaneel <bradleyoosterveen@gmail.com>
2020-05-18 23:17:09 +0200

    Update request.php

--- app/core/request.php
+++ app/core/request.php
@@ -1,4 +1,8 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
     if(isset($router->page)) {
         $request = json_decode(file_get_contents("php://input"));
     } else {
